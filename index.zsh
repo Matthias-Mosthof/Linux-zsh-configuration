45946b7344be0eff91c9e8e688afdd0af1adf7a3
Matthias Mosthof <matthias.mosthof@gmail.com>
2024-02-22 13:23:28 +0100

    adds history search bundle plugin

--- index.zsh
+++ index.zsh
@@ -9,8 +9,10 @@ antigen bundle zsh-users/zsh-syntax-highlighting
 antigen bundle zsh-users/zsh-autosuggestions
 antigen theme romkatv/powerlevel10k #you need to install powerline fonts for this theme (apt-get install powerline)
 antigen apply
+antigen bundle joshskidmore/zsh-fzf-history-search
 
 ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=4'
-
+export XDG_SESSION_TYPE=wayland
+export XDG_CURRENT_DESKTOP=sway
 
 [[ ! -f ~/.config/zsh/p10k.zsh ]] || source ~/.config/zsh/p10k.zsh
